882328854a37ebdc1d578023b4d565985d84ad2b
TaniaLap <be4535ap@gmail.com>
2024-08-29 00:11:42 +0200

    changed js

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
-// Відкрити модальне вікно
+// Отримуємо модальне вікно та елементи
 const modal = document.getElementById("modal");
 const modalImg = document.getElementById("modal-img");
+const closeBtn = document.querySelector(".close");
 
-document.querySelectorAll('.grid-item img').forEach(img => {
-    img.addEventListener('click', function() {
+// Всі зображення сітки
+const images = document.querySelectorAll(".grid-item img");
+
+// Додаємо подію кліку для кожного зображення
+images.forEach(img => {
+    img.addEventListener("click", function() {
         modal.style.display = "block";
         modalImg.src = this.src;
     });
 });
 
-// Закрити модальне вікно
-const closeBtn = document.getElementsByClassName("close")[0];
-
-closeBtn.onclick = function() {
+// Кнопка закриття модального вікна
+closeBtn.addEventListener("click", function() {
     modal.style.display = "none";
-}
+});
 
-// Закриття при натисканні за межами зображення
-window.onclick = function(event) {
+// Закриття модального вікна при кліку за межами зображення
+window.addEventListener("click", function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
-}
+});
