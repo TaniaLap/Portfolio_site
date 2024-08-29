f22aa1ab294fcdd3d4ddac78873675633b1365bd
TaniaLap <be4535ap@gmail.com>
2024-08-29 09:37:07 +0200

    update all

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
     var grid = document.querySelector('.grid');
-    var msnry = new Masonry(grid, {
-        itemSelector: '.grid-item',
-        columnWidth: '.grid-item',
-        percentPosition: true,
-        gutter: 10 // Відступ між елементами
+    
+    // Перевірка, чи всі зображення завантажились
+    imagesLoaded(grid, function() {
+        var msnry = new Masonry(grid, {
+            itemSelector: '.grid-item',
+            columnWidth: '.grid-item',
+            percentPosition: true,
+            gutter: 10
+        });
     });
 });
