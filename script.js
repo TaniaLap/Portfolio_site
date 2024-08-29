eb5c23a5f2d6744b17988abf11cd3528136adb91
TaniaLap <be4535ap@gmail.com>
2024-08-29 09:14:00 +0200

    css js

--- script.js
+++ script.js
@@ -1,27 +1,11 @@
-// Отримуємо модальне вікно та елементи
-const modal = document.getElementById("modal");
-const modalImg = document.getElementById("modal-img");
-const closeBtn = document.querySelector(".close");
-
-// Всі зображення сітки
-const images = document.querySelectorAll(".grid-item img");
-
-// Додаємо подію кліку для кожного зображення
-images.forEach(img => {
-    img.addEventListener("click", function() {
-        modal.style.display = "block";
-        modalImg.src = this.src;
+// Ініціалізація Masonry після завантаження всіх зображень
+document.addEventListener('DOMContentLoaded', function() {
+    var grid = document.querySelector('.portfolio');
+    var msnry = new Masonry(grid, {
+        // options
+        itemSelector: '.grid-item',
+        columnWidth: '.grid-item',
+        percentPosition: true,
+        gutter: 10
     });
-});
-
-// Кнопка закриття модального вікна
-closeBtn.addEventListener("click", function() {
-    modal.style.display = "none";
-});
-
-// Закриття модального вікна при кліку за межами зображення
-window.addEventListener("click", function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
 });
