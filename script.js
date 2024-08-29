6ca95f43bf899c59eacbeaae443dba20e0c02e0f
TaniaLap <be4535ap@gmail.com>
2024-08-29 09:41:13 +0200

    all

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     var grid = document.querySelector('.grid');
     
-    // Перевірка, чи всі зображення завантажились
     imagesLoaded(grid, function() {
         var msnry = new Masonry(grid, {
             itemSelector: '.grid-item',
-            columnWidth: '.grid-item',
             percentPosition: true,
             gutter: 10
         });
